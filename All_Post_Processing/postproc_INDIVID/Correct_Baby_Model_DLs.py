d6750c32e46fddd70b34ccf22da854e750ce9571
Mike Fienen <mnfienen@usgs.gov>
2012-02-23 11:12:52 -0600

    Inverse mills corrections for NDs in baby model

---
+++ All_Post_Processing/postproc_INDIVID/Correct_Baby_Model_DLs.py
@@ -0,0 +1,28 @@
+import numpy as np
+from inv_mills_corrections import Inverse_Mills_Ratio_Correction
+
+# load all the data
+infile = 'LOO_Baby_Model_w_DL.dat'
+
+indat = np.genfromtxt(infile,names=True,dtype=None)
+allsigma = indat['sigma']
+allHgmod = indat['Hg_LOO']
+allHgobs = indat['Hg_obs']
+allDL = indat['DL']
+
+inlines = open(infile,'r').readlines()
+header = inlines.pop(0).strip() + ' Hg_corrected\n'
+
+ofp = open(infile + 'corr.dat','w')
+ofp.write(header)
+
+numlines = len(indat)
+
+for i,cline in enumerate(inlines):
+    print 'processing %d of %d' %(i,numlines)
+    if allDL[i] == 1:
+        HgCorr,junkus = Inverse_Mills_Ratio_Correction(allHgobs[i],allsigma[i],allHgmod[i])
+    else:
+        HgCorr = allHgmod[i]
+    ofp.write(cline.strip() + ' %f\n' %(HgCorr))
+ofp.close()
